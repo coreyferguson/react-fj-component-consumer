19bffd2af8cdfec7e5c34dfc906ac48d6e2475fb
fjatoi <Fahar_Jatoi@intuit.com>
2017-08-16 18:08:43 -0700

    Demonstrating both components

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 //good for  testing components
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+import {HelloReactComponent} from 'react-fj-component';
 
+function run() {
+    ReactDOM.render(<HelloReactComponent />, document.getElementById('fr'));
+}
+
+window.onload = run;
